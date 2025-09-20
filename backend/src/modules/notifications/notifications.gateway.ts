8bf1d54ff2a6d8c102fafa7ac687d7ba0aad04dc
rahin21 <mdrahinzaman21@gmail.com>
2025-09-20 22:38:01 +0600

    cors edited

--- backend/src/modules/notifications/notifications.gateway.ts
+++ backend/src/modules/notifications/notifications.gateway.ts
@@ -4,7 +4,7 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3002', 'http://127.0.0.1:3002'],
+    origin: ['https://project-management-tool-beige.vercel.app/', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3002', 'http://127.0.0.1:3002'],
     credentials: true,
   },
   allowEIO3: true,
--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; // Use localhost instead of 0.0.0.0
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; 
 if (!API_BASE_URL) {
   throw new Error('NEXT_PUBLIC_API_URL is not defined');
 }
